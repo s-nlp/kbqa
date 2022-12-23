e5a367a502293ba54c3d8b8451e69fafc9207ec9
Mikhail Salnikov <Mikhail.Salnikov@skoltech.ru>
2022-12-23 18:23:51 +0300

    Candidate Selection by Direct Connections added as a submodule to kbqa and deploy

--- deploy/telegram_bot/main.py
+++ deploy/telegram_bot/main.py
@@ -13,17 +13,15 @@ from telegram.ext import (
 from telegram import ChatAction
 from label_to_entity import LabelToEntity
 import requests
-import time
 import os
-from functools import lru_cache
 import logging
 
 
 CANDIDATE_GENERATION_URI = os.getenv(
     "CANDIDATE_GENERATION_URI", "http://localhost:7860"
 )
-
 ENTITY_LINKING_URI = os.getenv("ENTITY_LINKING_URI", "http://localhost:7861")
+CANDIDATE_SELECTION_URI = os.getenv("CANDIDATE_SELECTION_URI", "http://localhost:7862")
 SPARQL_ENDPOINT = "https://query.wikidata.org/sparql"
 
 
@@ -43,88 +41,6 @@ def get_module_logger(mod_name):
 
 
 logger = get_module_logger("tg_bot")
-
-
-@lru_cache(maxsize=16384)
-def wikidata_request(query):
-    logger.info(f"Request to Wikidata with query:\n{query}")
-    response = requests.get(
-        SPARQL_ENDPOINT,
-        params={"format": "json", "query": query},
-        headers={"Accept": "application/json"},
-    )
-    to_sleep = 0.2
-    while response.status_code == 429:
-        if "retry-after" in response.headers:
-            to_sleep += int(response.headers["retry-after"])
-        to_sleep += 0.5
-        logger.info(f"wikidata_request to sleep...")
-        time.sleep(to_sleep)
-        response = requests.get(
-            SPARQL_ENDPOINT,
-            params={"format": "json", "query": query},
-            headers={"Accept": "application/json"},
-        )
-    return response.json()["results"]["bindings"]
-
-
-@lru_cache(maxsize=16384)
-def wikidata_get_count_of_intersections(entity1, entity2):
-    query = """
-SELECT (count(distinct ?p) as ?count) WHERE {
-  {wd:<E1> ?p wd:<E2>} UNION {wd:<E2> ?p wd:<E1>}
-}
-    """.replace(
-        "<E1>", entity1
-    ).replace(
-        "<E2>", entity2
-    )
-
-    count = wikidata_request(query)
-    return int(count[0]["count"]["value"])
-
-
-def wikidata_get_corresponded_objects(entity):
-    query = """
-SELECT ?p ?item WHERE {
-    wd:<E1> ?p ?item .
-    ?article schema:about ?item .
-    ?article schema:inLanguage "en" .
-    ?article schema:isPartOf <https://en.wikipedia.org/> .
-    SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-}
-    """.replace(
-        "<E1>", entity
-    )
-
-    cor_objects = wikidata_request(query)
-    cor_objects = [val["item"]["value"].split("/")[-1] for val in cor_objects]
-
-    return cor_objects
-
-
-def filter_candidates(candidate_ids, q_entity_ids):
-    final_list = []
-    for qentity in q_entity_ids:
-        for candidate in candidate_ids:
-            if wikidata_get_count_of_intersections(qentity, candidate) > 0:
-                final_list.append(candidate)
-                logger.info(f"{qentity} and {candidate} has direct path")
-                break
-    return list(dict.fromkeys(final_list))
-
-
-def filter_candidates_second_order(candidate_ids, q_entity_ids):
-    final_list = []
-    for qentity in q_entity_ids:
-        for candidate in candidate_ids:
-            for cor_obj in wikidata_get_corresponded_objects(qentity):
-                if wikidata_get_count_of_intersections(cor_obj, candidate) > 0:
-                    final_list.append(candidate)
-                    logger.info(f"{qentity} and {candidate} has direct path")
-                    break
-        return list(dict.fromkeys(final_list))
-
 
 logger.info("Loading LabelToEntity start...")
 label2entity = LabelToEntity()
@@ -167,6 +83,30 @@ def get_entity_linking_response(question):
     logger.info("entity_linker_responce: " + str(entity_linker_responce))
     entity_linked_results = entity_linker_responce["data"][0]
     return entity_linked_results
+
+
+def get_direct_connection_selection(answer_candidates_ids, entity_linked_ids, version):
+    if version == "one_hop":
+        uri = (
+            f"{CANDIDATE_SELECTION_URI}/relation_selection/one_hop_direct_connections/"
+        )
+    else:
+        uri = (
+            f"{CANDIDATE_SELECTION_URI}/relation_selection/two_hop_direct_connections/"
+        )
+
+    responce = requests.get(
+        uri,
+        json={
+            "question_entities_ids": entity_linked_ids,
+            "candidates_ids": answer_candidates_ids,
+        },
+        headers={
+            "Content-Type": "application/json",
+        },
+    ).json()
+    logger.info("get_direct_connection_selection_responce: " + str(responce))
+    return responce
 
 
 def get_entity_id_by_label(label):
@@ -215,7 +155,9 @@ def respond_to_user(update: Update, context: CallbackContext):
     )
 
     update.message.chat.send_action(action=ChatAction.TYPING)
-    filtered_candidates = filter_candidates(answer_candidates_ids, entity_linked_ids)
+    filtered_candidates = get_direct_connection_selection(
+        answer_candidates_ids, entity_linked_ids, "one_hop"
+    )
     final_answer_text = str(filtered_candidates)
     update.message.reply_text(
         f"FINAL ANSWER First Order for Question:\n** {question} **:\n\n"
@@ -223,8 +165,8 @@ def respond_to_user(update: Update, context: CallbackContext):
     )
 
     update.message.chat.send_action(action=ChatAction.TYPING)
-    filtered_candidates = filter_candidates_second_order(
-        answer_candidates_ids, entity_linked_ids
+    filtered_candidates = get_direct_connection_selection(
+        answer_candidates_ids, entity_linked_ids, "two_hop"
     )
     final_answer_text = str(filtered_candidates)
     update.message.reply_text(
--- kbqa/wikidata/utils.py
+++ kbqa/wikidata/utils.py
@@ -1,18 +1,26 @@
 import time
 from functools import lru_cache
+from joblib import Memory
 
 import requests
 
-from ..config import DEFAULT_LRU_CACHE_MAXSIZE, SPARQL_ENDPOINT
+from ..config import DEFAULT_LRU_CACHE_MAXSIZE, SPARQL_ENDPOINT, DEFAULT_CACHE_PATH
 from ..logger import get_logger
 
 
 logger = get_logger()
 
+memory = Memory(DEFAULT_CACHE_PATH, verbose=0)
+
 
 @lru_cache(maxsize=DEFAULT_LRU_CACHE_MAXSIZE)
+@memory.cache
 def request_to_wikidata(query, sparql_endpoint=SPARQL_ENDPOINT):
     params = {"format": "json", "query": query}
+    headers = {
+        "Accept": "application/json",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36",
+    }
     logger.info(
         {
             "msg": "Send request to Wikidata",
@@ -23,19 +31,20 @@ def request_to_wikidata(query, sparql_endpoint=SPARQL_ENDPOINT):
     response = requests.get(
         sparql_endpoint,
         params=params,
-        headers={"Accept": "application/json"},
+        headers=headers,
     )
     to_sleep = 0.2
     while response.status_code == 429:
         logger.warning(
             {
-                "msg": "Request to wikidata endpoint failed",
+                "msg": f"Request to wikidata endpoint failed. Retry.",
                 "params": params,
                 "endpoint": sparql_endpoint,
                 "response": {
                     "status_code": response.status_code,
                     "headers": dict(response.headers),
                 },
+                "retry_after": to_sleep,
             }
         )
         if "retry-after" in response.headers:
@@ -45,6 +54,19 @@ def request_to_wikidata(query, sparql_endpoint=SPARQL_ENDPOINT):
         response = requests.get(
             sparql_endpoint,
             params=params,
-            headers={"Accept": "application/json"},
+            headers=headers,
         )
-    return response.json()["results"]["bindings"]
+
+    try:
+        return response.json()["results"]["bindings"]
+    except Exception as e:
+        logger.error(
+            {
+                "msg": str(e),
+                "response": {
+                    "status_code": response.status_code,
+                    "headers": dict(response.headers),
+                },
+            }
+        )
+        raise e
